b5bbec816e3b69ded5a239c7cb83d1f29e196d44
karimjamali <karim.jamali@gmail.com>
2022-06-07 22:38:54 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
-# Configure Aviatrix provider source and version
-terraform {
-  required_providers {
-    aviatrix = {
-      source = "AviatrixSystems/aviatrix"
-      
-    }
-  }
-}
 
 provider "aviatrix" {
   controller_ip           = var.avx_controller_ip
@@ -19,16 +10,13 @@ provider "aviatrix" {
 #You need to input your access and secret key and decide on which region
 provider "aws" {
   region = var.aws_region_1
-  access_key = ""
-  secret_key = ""
+  access_key = var.aws_acess_key
+  secret_key = var.aws_secret_key
   
 }
 #You need to input your access and secret key and decide on which region
 provider "aws" {
   alias  = "west"
   region = var.aws_region_2
-  access_key = ""
-  secret_key = ""
-  
-}
-
+  access_key = var.aws_acess_key
+  secret_key = var.aws_secret_key
